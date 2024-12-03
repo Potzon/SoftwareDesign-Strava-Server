77d9744768c9b3138c3f6d3e3ab11e3def364b44
Potzon <dekumalditonerd@gmail.com>
2024-12-03 22:01:02 +0100

    fix abstract

--- src/main/java/com/example/strava/external/LoginServiceGateway.java
+++ src/main/java/com/example/strava/external/LoginServiceGateway.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import com.example.strava.dto.CredentialsDTO;
 import com.example.strava.entity.Credentials;
 
-public abstract interface LoginServiceGateway {
+public interface LoginServiceGateway {
 	public Optional<String> externalLogin(Credentials credentials);
 
 }
